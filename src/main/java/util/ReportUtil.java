cc6fdb2443a513aff57a1ddb58f54b88471c84f5
Leandro Amorim <leandroamorimpb@gmail.com>
2022-02-12 00:49:56 -0300

    Criando o pacote util e criando nossa classe ReportUtil

---
+++ src/main/java/util/ReportUtil.java
@@ -0,0 +1,38 @@
+package util;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+
+import org.apache.commons.collections.map.HashedMap;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class ReportUtil implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public byte[] geraRelatorioPdf(List listaDados, String nomeRelatorio, ServletContext servletContext) throws Exception {
+		
+		/**
+		 * Cria a lista de dados que vem do nosso SQL da consulta feita
+		 */
+		JRBeanCollectionDataSource jrbcds = new JRBeanCollectionDataSource(listaDados);
+		
+		String caminhoJasper = servletContext.getRealPath("relatorio") + File.separator + nomeRelatorio + ".jasper";
+		
+		JasperPrint impressoraJasper = JasperFillManager.fillReport(caminhoJasper, new HashMap(), jrbcds);
+		
+		return JasperExportManager.exportReportToPdf(impressoraJasper);
+		
+		
+	}
+}
